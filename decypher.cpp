daf987c93060ef7bf00dc3bbdcd50b1138c6846b
Anton <budowniczy.zamyslow@gmail.com>
2016-05-11 20:24:56 +0500

    Update decypher.cpp

--- decypher.cpp
+++ decypher.cpp
@@ -2,5 +2,20 @@
 
 string Decypher::vigenere(const string text, const string key)
 {
-    return "tre";
+    if (text.empty() || key.empty())
+        return text;
+
+    string decyphered;
+    for (int i = 0; i < text.length(); i++)
+        decyphered += (isalpha(text[i]))
+            ? vigenere(text[i], key[i % key.length()])
+            : text[i];
+
+    return decyphered;
+}
+
+char Decypher::vigenere(const char c, const char keyC)
+{
+    char decyphered = 'a' + (tolower(c) - keyC + alphabetSize) % alphabetSize;
+    return (isupper(c)) ? toupper(decyphered) : decyphered;
 }
